3e4c1d90eeaeab5d5963eca605bced087dd9f8d5
Acuf5928 <alberto.cuffaro@hotmail.it>
2017-03-22 18:07:04 +0100

    Piccole migliorie
    Ora verifica che il file sia ".c". Se l'estensione non corrisponde il programma si riavvia

--- main.py
+++ main.py
@@ -6,19 +6,25 @@ percorsofile = ""
 Selto = "0"
 commenti = ""
 
-while percorsofile!="-1": 
-    percorsofile = input("Percosso file da compilare con gcc: ")
+while percorsofile!="-1":
+    while percorsofile!="-1": 
+        percorsofile = input("Percosso file da compilare con gcc: ")
 
-    Scelta = input("Vuoi inserire commenti al compilatore?\n 1)Si 2)No\n")
+        if percorsofile[-1]!="c":
+            if percorsofile[-2]!="c":
+                print("File non valido")
+                break
+        
+        Scelta = input("Vuoi inserire commenti al compilatore?\n 1)Si 2)No\n")
 
-    if Scelta == "1":
-        commenti = input("Inserire commento:")
+        if Scelta == "1":
+            commenti = input("Inserire commento:")
     
-    if percorsofile == "-1":
-        break
+        if percorsofile == "-1":
+            break
 
-    salvataggio = percorsofile.replace('.c', '.exe')
+        salvataggio = percorsofile.replace('.c', '.exe')
 
-    os.system("gcc -o " + salvataggio + " " + commenti + " " + percorsofile)
+        os.system("gcc -o " + salvataggio + " " + commenti + " " + percorsofile)
 
-    Scelta = "0"
+        Scelta = "0"
