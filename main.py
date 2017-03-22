1a17b59d8864d35438fb78c5f02bf1fc96a4af45
Acuf5928 <alberto.cuffaro@hotmail.it>
2017-03-22 16:54:53 +0100

    Aggiunta possibilità di inserire commenti

--- main.py
+++ main.py
@@ -3,14 +3,22 @@
 import os
 
 percorsofile = ""
+Selto = "0"
+commenti = ""
 
 while percorsofile!="-1": 
     percorsofile = input("Percosso file da compilare con gcc: ")
 
+    Scelta = input("Vuoi inserire commenti al compilatore?\n 1)Si 2)No\n")
+
+    if Scelta == "1":
+        commenti = input("Inserire commento:")
+    
     if percorsofile == "-1":
         break
 
     salvataggio = percorsofile.replace('.c', '.exe')
 
-    os.system("gcc -std=c99 -o " + salvataggio + " " + percorsofile)
+    os.system("gcc -o " + salvataggio + " " + commenti + " " + percorsofile)
 
+    Scelta = "0"
